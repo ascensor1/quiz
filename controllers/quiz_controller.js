2f241bf28376ea474c0454251d5824ae72b4115f
Victor Romero Blanco <victoromerob@gmail.com>
2015-06-25 00:08:29 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,32 +1,51 @@
 var models = require('../models/models.js');
 
 
-// GET /quizes/question
-exports.question = function(req, res) {
-// 	res.render('quizes/question', {pregunta: 'Capital de Italia',title: 'Pregunta'});
+// // GET /quizes/question
+// exports.question = function(req, res) {
+// // 	res.render('quizes/question', {pregunta: 'Capital de Italia',title: 'Pregunta'});
+// 	models.Quiz.findAll().then(function(quiz) {
+// 		res.render('quizes/question', { pregunta: quiz[0].pregunta});
+// 	})
+// };
+
+
+// GET /quizes
+exports.index = function(req, res) {
 // 	models.Quiz.findAll().success(function(quiz) {
-	models.Quiz.findAll().then(function(quiz) {
-		res.render('quizes/question', { pregunta: quiz[0].pregunta});
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index.ejs', { quizes: quizes});
 	})
 };
 
 // GET /quizes/answer
-exports.answer = function(req, res) {
+// exports.answer = function(req, res) {
 // 	if (req.query.respuesta === 'Roma'){
 // 		res.render('quizes/answer', {respuesta: 'Correcto',title: 'Respuesta'});
 // 	} else {
 // 		res.render('quizes/answer', {respuesta: 'Incorrecto. La respuesta era "Roma"',title: 'Respuesta'});
 // 	}
-// 	models.Quiz.findAll().success(function(quiz) {
-	models.Quiz.findAll().then(function(quiz) {
-		if (req.query.respuesta === quiz[0].respuesta) {
-			res.render('quizes/answer', { respuesta: 'Correcto'});
-		} else {
-			res.render('quizes/answer', { respuesta: 'Incorrecto'});
-		}
-	})
-	
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    res.render('quizes/show', { quiz: quiz});
+  })
 };
+
+
+// GET /quizes/:id/answer
+ exports.answer = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    if (req.query.respuesta === quiz.respuesta) {
+      res.render('quizes/answer', 
+                 { quiz: quiz, respuesta: 'Correcto' });
+     } else {
+      res.render('quizes/answer', 
+                 { quiz: quiz, respuesta: 'Incorrecto'});
+     }
+   })
+ };
 
 // GET /author
 exports.author = function(req, res) {
