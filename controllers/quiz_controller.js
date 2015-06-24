d1d697f68ee4ef87cfa92731dcd6690d303ab8f6
Victor Romero Blanco <victoromerob@gmail.com>
2015-06-24 23:49:30 +0200

    Modelo y base de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,33 @@
+var models = require('../models/models.js');
+
 // GET /quizes/question
 exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia',title: 'Pregunta'});
+// 	res.render('quizes/question', {pregunta: 'Capital de Italia',title: 'Pregunta'});
+	models.Quiz.findAll().success(function(quiz) {
+		res.render('quizes/question', { pregunta: quiz[0].pregunta});
+	})
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-	if (req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto',title: 'Respuesta'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto. La respuesta era "Roma"',title: 'Respuesta'});
-	}
+// 	if (req.query.respuesta === 'Roma'){
+// 		res.render('quizes/answer', {respuesta: 'Correcto',title: 'Respuesta'});
+// 	} else {
+// 		res.render('quizes/answer', {respuesta: 'Incorrecto. La respuesta era "Roma"',title: 'Respuesta'});
+// 	}
+
+	models.Quiz.findAll().success(function(quiz) {
+		if (req.query.respuesta === quiz[0].respuesta) {
+			res.render('quizes/answer', { respuesta: 'Correcto'});
+		} else {
+			res.render('quizes/answer', { respuesta: 'Incorrecto'});
+		}
+	})
+	
 };
 
 // GET /author
 exports.author = function(req, res) {
-	res.render("author", {title: "Créditos"});
+	res.render("author", {});
 };
+
